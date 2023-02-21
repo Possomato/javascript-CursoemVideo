58324cdc9acc81aa00a465a052d8d1cc3b6596db
Gabriel Bonifacio Possomato <gabrielb.possomato@gmail.com>
2023-02-21 15:21:09 -0300

    add ex016/017

---
+++ ambiente/ambiente.js
@@ -0,0 +1,23 @@
+var c = 1
+
+// console.log('1')
+// console.log('2')
+// console.log('3')
+// console.log('4')
+// console.log('5')
+// console.log('6')
+
+
+// while (c <= 6){
+//     console.log(c)
+//     c += 1
+// }
+
+// do{
+//     console.log(c)
+//     c += 1
+// } while (c <=6)
+
+for (var c = 1; c <=6; c += 1){
+    console.log(c)
+}
