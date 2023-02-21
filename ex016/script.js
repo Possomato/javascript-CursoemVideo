c99c868467e3b9f798cecabb0267bb68cadc3f0c
Gabriel Bonifacio Possomato <gabrielb.possomato@gmail.com>
2023-02-21 19:34:54 -0300

    att ex016

--- ex016/script.js
+++ ex016/script.js
@@ -4,5 +4,27 @@ function contar(){
     var pas = window.document.getElementById('tpas')
     var res = window.document.getElementById('res')
 
-    
+    if (ini.value.length == 0 || fim.value.length == 0 || pas.value.length == 0){
+        alert('[ERRO] faltam dados!')
+    } else {
+        res.innerHTML = 'Contando: '
+        var i = Number(ini.value)
+        var f = Number(fim.value)
+        var p = Number(pas.value)
+        if (p <= 0){
+            alert('[ERRO] passo inválido! Considerando passo = 1')
+            p = 1
+        }
+        if (i < f){
+            for(var c = i; c <= f; c += p){
+                res.innerHTML += `${c} \u{27A1}`
+            }
+
+        } else {
+            for(var c = i; c >= f; c -= p){
+                res.innerHTML += `${c} \u{27A1}`
+            }
+        }
+        res.innerHTML += `\u{1F3C1}`
+    }
 }
