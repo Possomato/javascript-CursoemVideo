ca8e8427f53363debc4a4ebeed41086e9b4dc248
Gabriel Bonifacio Possomato <gabrielb.possomato@gmail.com>
2023-02-21 18:38:10 -0300

    add ex016/ex017

---
+++ ex016/script.js
@@ -0,0 +1,8 @@
+function contar(){
+    var ini = window.document.getElementById('tini')
+    var fim = window.document.getElementById('tfim')
+    var pas = window.document.getElementById('tpas')
+    var res = window.document.getElementById('res')
+
+    
+}
---
+++ ex017/script.js
@@ -0,0 +1,4 @@
+function gerar(){
+    var res = window.document.getElementById('res')
+    res.innerHTML = 'teste'
+}
