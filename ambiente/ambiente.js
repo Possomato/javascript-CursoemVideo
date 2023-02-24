7bedcec00d5765dee07b88e74d30cf3d566ed75d
Gabriel Bonifacio Possomato <gabrielb.possomato@gmail.com>
2023-02-23 21:26:03 -0300

    att ambiente

--- ambiente/ambiente.js
+++ ambiente/ambiente.js
@@ -1,4 +1,4 @@
-var c = 1
+// var c = 1
 
 // console.log('1')
 // console.log('2')
@@ -18,6 +18,14 @@ var c = 1
 //     c += 1
 // } while (c <=6)
 
-for (var c = 1; c <=6; c += 1){
-    console.log(c)
-}
+// for (var c = 1; c <=6; c += 1){
+//     console.log(c)
+// }
+
+let num = [5, 66, 7, 9]
+
+num.push(1)
+num.sort()
+console.log(num)
+console.log(`Nosso vetor tem ${num.length} posições`)
+console.log(`O primeiro valor do vetor é ${num[0]}`)
